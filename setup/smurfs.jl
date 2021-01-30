5ae0a6f861801b1d61679132e0cf29c517a0ec61
Thalassocracy <reckoner.info@gmail.com>
2021-01-30 17:59:55 -0500

    Update setup/smurfs.jl to allow for main_id changes

--- setup/smurfs.jl
+++ setup/smurfs.jl
@@ -13,11 +13,12 @@ end
 
 
 LibPQ.execute(conn, "BEGIN;")
+LibPQ.execute(conn, "DELETE FROM reckoner.smurfs WHERE alt_player_type != 'river';")
 LibPQ.load!(
     (alts = data.alt_player_id, mains = data.main_player_id,
     alt_types = data.alt_player_type, main_types = data.main_player_type), conn,
     "INSERT INTO reckoner.smurfs (alt_player_id, main_player_id, alt_player_type, main_player_type)
-    VALUES (\$1, \$2, \$3, \$4) ON CONFLICT DO NOTHING;")
+    VALUES (\$1, \$2, \$3, \$4);")
 LibPQ.execute(conn, "COMMIT;")
 
 close(conn)
