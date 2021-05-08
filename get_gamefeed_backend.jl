1cc41c8cdbff89afeb153300616a3798c2974c91
Thalassocracy <reckoner.info@gmail.com>
2021-05-08 19:29:49 -0400

    Fortify get_gamefeed.jl from SQL injections

--- get_gamefeed_backend.jl
+++ get_gamefeed_backend.jl
@@ -9,6 +9,18 @@ include("reckoner_common.jl")
 const GAMEFEED_TIME_FORMAT = Dates.DateFormat("yyyy-mm-dd.HH:MM:SS")
 
 function process_gamefeed(gamefeed_input, conn)
+    insert_gamefeed_ps = LibPQ.prepare(conn, "
+        INSERT INTO reckoner.gamefeed (
+            lobbyid, obs_time, ffa,
+            mods, bounty, system_name,
+            system_info_gamefeed, QBE, 
+            sandbox
+        ) VALUES (
+            \$1, \$2, \$3, \$4, \$5, 
+            \$6, \$7, \$8, \$9
+        ) ON CONFLICT (lobbyid) DO NOTHING;
+    ")
+
     gamefeed = JSON.parse(String(gamefeed_input))
 
     obstime::Timestamp = floor(Timestamp, Dates.datetime2unix(Dates.DateTime(gamefeed["BackendTime"], GAMEFEED_TIME_FORMAT)))
@@ -27,35 +39,17 @@ function process_gamefeed(gamefeed_input, conn)
                                     else
                                         0.0
                                     end
-                system_info_gamefeed::String = sanitize(JSON.json(mdata["game"]["system"]["planets"]))
-                system_name::String = sanitize(mdata["game"]["system"]["name"])
+                system_info_gamefeed::String = JSON.json(mdata["game"]["system"]["planets"])
+                system_name::String = mdata["game"]["system"]["name"]
 
-                mod_ids = mdata["mod_identifiers"]
-                QBE = ("com.pa.quitch.AIBugfixEnhancement" in mod_ids || "com.pa.quitch.AIBugfixEnhancement-dev" in mod_ids)
-                mods::String = format_array_postgres(sanitize.(mod_ids))
+                mods::Array{String} = mdata["mod_identifiers"]
+                QBE = ("com.pa.quitch.AIBugfixEnhancement" in mods || "com.pa.quitch.AIBugfixEnhancement-dev" in mods)
 
-                if sandbox
-                    query::String = "   INSERT INTO reckoner.gamefeed
-                                        (lobbyid, obs_time, sandbox)
-                                        VALUES
-                                        ($lobbyid, $obstime, $sandbox)
-                                        ON CONFLICT DO NOTHING;
-                                        "
-                    LibPQ.execute(conn, query)
-                else
-                    query = "   INSERT INTO reckoner.gamefeed
-                                (lobbyid, obs_time, ffa,
-                                mods, bounty, system_name,
-                                system_info_gamefeed, QBE)
-                                VALUES
-                                ($lobbyid, $obstime, $ffa,
-                                $mods, $bounty, '$system_name',
-                                '$system_info_gamefeed', $QBE)
-                                ON CONFLICT (lobbyid) DO UPDATE
-                                SET bounty = $bounty;
-                                "
-                    LibPQ.execute(conn, query)
-                end
+                LibPQ.execute(insert_gamefeed_ps, (
+                    lobbyid, obstime, ffa, mods, bounty, 
+                    system_name, system_info_gamefeed, QBE, 
+                    sandbox
+                ))
             end
         end
     end
