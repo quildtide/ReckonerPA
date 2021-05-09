0d62e6e99a9073da47e8ddb1aec43f771d16592b
Thalassocracy <reckoner.info@gmail.com>
2021-05-08 22:29:04 -0400

    Fortify rating apis from SQL injections

--- site.jl
+++ site.jl
@@ -7,6 +7,8 @@ const ROUTER = HTTP.Router()
 
 conn = LibPQ.Connection("dbname=reckoner user=reckoner")
 
+const player_hist_ps = prepare_player_matches_ps(conn)
+
 function hallo(req::HTTP.Request)
     print("hallo\n")
     return HTTP.Response(200, "HALLO WARLD")
@@ -42,7 +44,7 @@ function basic_rating(req::HTTP.Request)
     if uberid isa Nothing
         message::String = "INVALID PLAYER"
     else
-        message = simple_player_rating(uberid, conn)
+        message = simple_player_rating(player_hist_ps, uberid)
     end
 
     return HTTP.Response(200, message)
@@ -52,7 +54,7 @@ HTTP.@register(ROUTER, "GET", "/basic_rating/*", basic_rating)
 
 function full_rating(req::HTTP.Request)
     
-    message = full_player_rating(HTTP.URIs.queryparams(HTTP.URIs.URI(req.target))["context"], conn)
+    message = full_player_rating(player_hist_ps, HTTP.URIs.queryparams(HTTP.URIs.URI(req.target))["context"])
 
     # message = JSON.json(Dict("rating_means"=> Dict(8665259548474200874 => "Hello world")))
 
