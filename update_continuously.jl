54d28c5f0546334d0fbb644dac9e11d3ee85591f
Thalassocracy <reckoner.info@gmail.com>
2021-05-27 00:43:37 -0400

    Reduce chances of update script failing w/o restart

--- update_continuously.jl
+++ update_continuously.jl
@@ -7,37 +7,36 @@ include("evaluation/evaluate_matches.jl")
 while true
     try
         print(Dates.now(), '\n')
-    catch
-        println("failed to get Dates.now()")
-    end
 
-    conn = LibPQ.Connection("dbname=reckoner user=reckoner")
-
-    try
-        get_superstats(conn)
-    catch
-        print("failed superstats update\n")
-        LibPQ.close(conn)
         conn = LibPQ.Connection("dbname=reckoner user=reckoner")
-    end
 
-    try
-        get_replayfeed(conn)
-    catch
-        print("failed replayfeed update\n")
-        LibPQ.close(conn)
-        conn = LibPQ.Connection("dbname=reckoner user=reckoner")
-    end
-    
-    try
-        evaluate_matches(conn)
-    catch
-        print("failed evaluate matches\n")
+        try
+            get_superstats(conn)
+        catch
+            print("failed superstats update\n")
+            LibPQ.close(conn)
+            conn = LibPQ.Connection("dbname=reckoner user=reckoner")
+        end
+
+        try
+            get_replayfeed(conn)
+        catch
+            print("failed replayfeed update\n")
+            LibPQ.close(conn)
+            conn = LibPQ.Connection("dbname=reckoner user=reckoner")
+        end
         
+        try
+            evaluate_matches(conn)
+        catch
+            print("failed evaluate matches\n")
+        finally
+            LibPQ.close(conn)
+        end
+        print("updated\n")
+
+        sleep(600)
+    catch
+        sleep(200)
     end
-    print("updated\n")
-
-    LibPQ.close(conn)
-
-    sleep(600)
 end
