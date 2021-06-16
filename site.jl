22493aceb9a877c304549bcf9a0da497f7aeea58
Thalassocracy <reckoner.info@gmail.com>
2021-06-16 00:36:12 -0400

    Add cached_ratings API to site

--- site.jl
+++ site.jl
@@ -1,7 +1,9 @@
 import HTTP
 import Sockets
+import Dates: datetime2unix
 
 include("evaluation/site_functions.jl")
+include("reckoner_common.jl")
 
 const ROUTER = HTTP.Router()
 
@@ -9,12 +11,16 @@ conn = LibPQ.Connection("dbname=reckoner user=reckoner")
 
 const player_hist_ps = prepare_player_matches_ps(conn)
 
+const STMT_RATING_CACHE = LibPQ.prepare(conn, "
+    SELECT * FROM reckoner.last_rating
+    WHERE player_type = ANY(\$1)
+    AND player_id = ANY(\$2)
+")
+
 function hallo(req::HTTP.Request)
     print("hallo\n")
     return HTTP.Response(200, "HALLO WARLD")
 end
-
-
 
 function echo_req(req::HTTP.Request)
     message::String = HTTP.URIs.splitpath(req.target)[2]
@@ -62,6 +68,38 @@ function full_rating(req::HTTP.Request)
 end
 
 HTTP.@register(ROUTER, "GET", "/api/full_rating", full_rating)
+
+function cached_rating(req::HTTP.Request)
+    context = JSON.parse(HTTP.URIs.queryparams(HTTP.URIs.URI(req.target))["context"])
+    player_types = context["player_types"]
+    player_ids = context["player_ids"]
+
+    res = STMT_RATING_CACHE(player_types, player_ids)
+
+    out = Dict("ratings" => Vector())
+
+    for row in res
+        player_rating = Dict(
+            "player_type" => row.player_type,
+            "player_id" => row.player_id,
+            "time_updated" => Dates.datetime2unix(row.time_updated),
+            "reckoner_version" => row.reckoner_version,
+            "rating_core" => row.rating_core,
+            "rating_1v1" => row.rating_1v1,
+            "rating_team" => row.rating_team,
+            "rating_ffa" => row.rating_ffa,
+            "rating_multiteam" => row.rating_multiteam,
+            "sd_1v1" => row.sd_1v1,
+            "sd_team" => row.sd_team,
+            "sd_ffa" => row.sd_ffa,
+            "sd_multiteam" => row.sd_multiteam
+        )
+        push!(out["ratings"], player_rating)
+    end
+
+    return HTTP.Response(200, JSON.json(out))
+end
+HTTP.@register(ROUTER, "GET", "/api/cached_rating", cached_rating)
 
 HTTP.serve(ROUTER, Sockets.IPv4("64.20.35.179"), 8085)
 
